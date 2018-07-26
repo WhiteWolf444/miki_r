c118b0d260d66b58b55df8e1f635a12be6e8d20b
Martin Nejedlý <nejedly.martin1@gmail.com>
2018-07-26 12:24:37 +0200

    z to y

--- Miki_trial.R
+++ Miki_trial.R
@@ -1,4 +1,4 @@
 # I love Miki <3
-print ("I love Miki very verz verz much <3")
+print ("I love Miki very very very much <3")
 Miki_vector <- c("I", "love", "Miki", "very", "much", "I", "want", "to", "cuddle", "him")
 matrix (Miki_vector, byrow = TRUE, nrow = 2)
